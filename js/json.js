a7ddf7bc496b73311988b3485ee8cdd3d1e6349e
Kevin Fry <kevinfry@fullsail.edu>
2013-09-30 01:22:45 -0400

    App copied over from VFW and set up in NetBeans

---
+++ js/json.js
@@ -0,0 +1,42 @@
+/* 
+ * Kevin Fry
+ * Full Sail - VFW1309 Project 4
+ * Pricebook
+ */
+
+
+var json = {
+    "item1": {
+        "location": ["Location:", "ACME"],
+        "productBrand": ["Brand:", "Goya"],
+        "productName": ["Name:", "Beans"],
+        "description": ["Description:", "ShopRite"],
+        "productRating": ["Rating:", "5"],
+        "price": ["Price:", "0.79"],
+        "tax": ["Taxable:", "Yes"],
+        "datePurchased": ["Date:", "07/26/2013"],
+        "size": ["Size:", "15 oz"]
+    },
+    "item2": {
+        "location": ["Location:", "ShopRite"],
+        "productBrand": ["Brand:", "Progresso"],
+        "productName": ["Name:", "Soup"],
+        "description": ["Description:", ""],
+        "productRating": ["Rating:", "3"],
+        "price": ["Price:", "1.25"],
+        "tax": ["Taxable:", "No"],
+        "datePurchased": ["Date:", "08/26/2013"],
+        "size": ["Size:", "15 oz"]
+    },
+    "item3": {
+        "location": ["Location:", "WalMart"],
+        "productBrand": ["Brand:", "Bush\'s"],
+        "productName": ["Name:", "Baked Beans"],
+        "description": ["Description:", "Veg"],
+        "productRating": ["Rating:", "5"],
+        "price": ["Price:", "0.99"],
+        "tax": ["Taxable:", "No"],
+        "datePurchased": ["Date:", "09/25/2013"],
+        "size": ["Size:", "24 oz"]
+    }
+};
